07e3304098abb75176b36a500b8ed1c26cf824e9
shilvia1912 <89684977+shilvia1912@users.noreply.github.com>
2023-07-30 20:23:49 +0700

    Add files via upload

---
+++ a.js
@@ -0,0 +1,51 @@
+// Để thêm nhạc bạn sao chép và chỉnh sửa bên trong đoạn array như sau
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên đường dẫn hình ảnh",
+//     src: "Tên đường dẫn theo bài nhạc"
+//   }
+let allMusic = [
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Schor",
+        img: "https://cdn.jsdelivr.net/gh/ngylduy/storage/image/music-1.jpg",
+        src: "https://ia801400.us.archive.org/27/items/music_20210917/music-1.mp3",
+        id: "m1"
+    },
+    {
+        name: "Ikson Anywhere – Ikson",
+        artist: "Audio Library",
+        img: "https://cdn.jsdelivr.net/gh/ngylduy/storage/image/music-2.jpg",
+        src: "https://ia801400.us.archive.org/27/items/music_20210917/music-2.mp3",
+        id: "m2"
+    },
+    {
+        name: "Beauz & Jvna - Crazy",
+        artist: "Beauz & Jvna",
+        img: "https://cdn.jsdelivr.net/gh/ngylduy/storage/image/music-3.jpg",
+        src: "https://ia801400.us.archive.org/27/items/music_20210917/music-3.mp3",
+        id: "m3"
+    },
+    {
+        name: "Hardwind - Want Me",
+        artist: "Mike Archangelo",
+        img: "https://cdn.jsdelivr.net/gh/ngylduy/storage/image/music-4.jpg",
+        src: "https://ia801400.us.archive.org/27/items/music_20210917/music-4.mp3",
+        id: "m4"
+    },
+    {
+        name: "Jim - Sun Goes Down",
+        artist: "Jim Yosef x Roy",
+        img: "https://cdn.jsdelivr.net/gh/ngylduy/storage/image/music-5.jpg",
+        src: "https://ia801400.us.archive.org/27/items/music_20210917/music-5.mp3",
+        id: "m5"
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NCS Release",
+        img: "https://cdn.jsdelivr.net/gh/ngylduy/storage/image/music-6.jpg",
+        src: "https://ia801400.us.archive.org/27/items/music_20210917/music-6.mp3",
+        id: "m6"
+    }
+];
